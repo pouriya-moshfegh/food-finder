e5addf972aa3aaba04f6f464b6ef90af1efeef5b
pouriya moshfegh <pouriya.moshfeghs@gmail.com>
2023-06-10 19:17:03 +0330

    handled server and client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,12 @@ export default function Home() {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar/>
-        <Header/>
+        <NavBar />
+        <Header />
         <main>
-          {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <Card/>
+            <Card />
           </div>
-          {/* CARDS */}
         </main>
       </main>
     </main>
